b960958efda5fbe1b651a963ef37a0b296372ac8
gitaldo98@gmail.com <newbie.tbn56@gmail.com>
2021-11-27 22:30:52 +0700

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import Brand from "./brand/Brand";
+import Navbar from './navbar/Navbar'
+import Feature from "./feature/Feature";
+import CTA from "./cta/CTA";
+export{Brand, Navbar, Feature,CTA}
---
+++ src/containers/index.js
@@ -0,0 +1,7 @@
+import Header from "./header/Header";
+import WhatGPT3 from "./whatGPT3/WhatGPT3";
+import Features from "./features/Features";
+import Possibility from "./possibility/Possibility";
+import Blog from "./blog/Blog";
+import Footer from "./footer/Footer";
+export { Header, WhatGPT3, Features, Possibility ,Blog, Footer};
